fbda1882c56cf48f424d49ebcf6440a68007c7b3
DDF Roylance <davisroylance@gmail.com>
2026-09-21 22:27:39 -0600

    Modify blog metadata for personal blog
    Updated blog metadata including title, URL, description, and author information.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Danny Roylance's Personal Blog",
+	url: "https://dannyroylance.github.io/Blog/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experience as a person who experiences.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Danny Roylance",
+		email: "davisroylance@gmail.com",
+		url: "https://dannyroylance.github.io/Blog/"
 	}
 }
